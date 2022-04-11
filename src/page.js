36459a43295c14b77c8a369dcc9ba11359bd5b16
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-04-10 20:31:40 -0400

    finished page.js code to generate HTML based on user's inputs

--- src/page.js
+++ src/page.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function generateCard(input) {
+function generateEmployee(input) {
     let unique;
     ({name, id, email, officeNumber, school, github} = input)
     
@@ -31,4 +31,53 @@ function generateCard(input) {
     </div>
     `
     return cardContent;
+}
+
+function generateCards(input) {
+    let employees = '';
+    for (i = 0; i < input.length; i++) {
+        employees += generateEmployee(input[i]);
+    }
+    return employees;
+}
+
+module.exports = function (input) {
+    let template = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="jumbotron text-center">
+                <h1>My Team</h1>
+            </div>
+        </div>
+    
+        <div class="container" id="employee-cards">
+
+            ${generateCards(input)}
+            
+        </div>
+    </body>
+    </html>
+    `
+
+    new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', template, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });          
+        });
+    });
 }
